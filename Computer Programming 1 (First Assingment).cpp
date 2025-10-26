09ee0e2903c1b29a43f052fe11e8bb20a86db6cf
tamzy-kfi <otd1feb2008@gmail.com>
2025-10-26 16:40:58 +0100

    Add files via upload

---
+++ Computer Programming 1 (First Assingment).cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+ int main(){
+ double num1, num2;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Calculate and display results
+    cout << "Sum = " << num1 + num2 << endl;
+    cout << "Difference = " << num1 - num2 << endl;
+    cout << "Product = " << num1 * num2 << endl;
+
+	return 0;
+} 
+
+#include <iostream>
+using namespace std;
+
+/* int main() {
+    int a, b, temp;
+
+    // Input two integers
+    cout << "Enter first integer: ";
+    cin >> a;
+
+    cout << "Enter second integer: ";
+    cin >> b;
+
+    // Display before swapping
+    cout << "\nBefore swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    // Swap using a temporary variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    // Display after swapping
+    cout << "\nAfter swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+} */
